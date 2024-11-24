b0f355f3be21c3581f8d2f5b135b73d67d312156
kali <MadMax@kali>
2024-11-24 23:40:29 +0100

    Initial commit for SAIDATA

--- saidata/saidata.py
+++ saidata/saidata.py
@@ -1,8 +1,8 @@
-from initDisplay import Display_Initial_Data
-from cpu import Display_CPU_Info
-from ram import Display_RAM_Info
-from disk import Display_DISK_Info
-from processes import Display_PROC_Info
+from saidata.initDisplay import Display_Initial_Data
+from saidata.cpu import Display_CPU_Info
+from saidata.ram import Display_RAM_Info
+from saidata.disk import Display_DISK_Info
+from saidata.processes import Display_PROC_Info
 
 def main():
     #? Displaying a welcome message:
