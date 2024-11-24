e7c7481a14882b327d59f0931c98fed188804fe9
kali <MadMax@kali>
2024-11-24 23:06:31 +0100

    Initial commit for SAIDATA

--- setup.py
+++ setup.py
@@ -2,20 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='saidata',
-    version='1.0',
+    version='1.0.0',
+    description='Mini-Dashboard Terminal pour la Surveillance du Système en Temps Réel',
+    author='Said Laachir',
     packages=find_packages(),
     install_requires=[
-        'psutil','rich', 'pynput','keyboard'  # Example dependency
+        'psutil',
+        'rich'
     ],
     entry_points={
         'console_scripts': [
-            'saidata = saidata.saidata:main',  # The command `saidata` will run the `main` function in `saidata.py`
+            'saidata=saidata.saidata:main',  # chemin vers votre fonction `main`
         ],
     },
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.6',
 )
